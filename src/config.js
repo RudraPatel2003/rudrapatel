ed4804d2b176e5e9314407bdc9e7f69828908153
Rudra Patel <patelrudra2003@gmail.com>
2022-07-28 11:32:08 -0500

    Updates productivist information

--- src/config.js
+++ src/config.js
@@ -8,40 +8,45 @@ export const contactInformation = {
 export const projects = [
   {
     name: "Portfolio Website",
-    description: "The website you are currently on right now. Built to showcase my personal projects and introduce myself!",
+    description:
+      "The website you are currently on right now. Built to showcase my personal projects and introduce myself!",
     tags: ["JavaScript", "React", "Next.js", "Styled Components"],
-    githubLink: "https://github.com/RudraPatel2003/rudrapatel"
+    githubLink: "https://github.com/RudraPatel2003/rudrapatel",
   },
   {
     name: "productivist",
-    description: "A responsive to-do list website built with Next.js and Tailwind CSS",
-    tags: ["JavaScript", "React", "Next.js", "Tailwind CSS"],
+    description:
+      "A responsive to-do list website built with Next.js and Tailwind CSS that incorporates user authentication through next-auth and MongoDB.",
+    tags: ["React", "Next.js", "Tailwind CSS", "next-auth", "MongoDB"],
     githubLink: "https://github.com/RudraPatel2003/productivist",
-    externalLink: "https://productivist.netlify.app/"
+    externalLink: "https://productivist.vercel.app/",
   },
   {
     name: "Sudoku Solver GUI",
-    description: "Play Sudoku on a pygame window and watch the Sudoku backtracking algorithm in action.",
+    description:
+      "Play Sudoku on a pygame window and watch the Sudoku backtracking algorithm in action.",
     tags: ["Python", "pygame", "GUI", "Algorithms"],
-    githubLink: "https://github.com/RudraPatel2003/Sudoku_Solver_GUI"
+    githubLink: "https://github.com/RudraPatel2003/Sudoku_Solver_GUI",
   },
   {
     name: "NYT Spelling Bee Solver",
-    description: "Launch a Python Selenium bot that plays the New York Times' Spelling Bee game.",
+    description:
+      "Launch a Python Selenium bot that plays the New York Times' Spelling Bee game.",
     tags: ["Python", "Selenium", "Web Scraping"],
-    githubLink: "https://github.com/RudraPatel2003/NYT_Spelling_Bee_Solver"
+    githubLink: "https://github.com/RudraPatel2003/NYT_Spelling_Bee_Solver",
   },
   {
     name: "Percy Warner Getaways",
     description: "View a responsive landing page for a nature tour company.",
     tags: ["JavaScript", "HTML", "CSS"],
     githubLink: "https://github.com/RudraPatel2003/Percy_Warner_Getaways",
-    externalLink: "https://rudrapatel2003.github.io/Percy_Warner_Getaways/"
+    externalLink: "https://rudrapatel2003.github.io/Percy_Warner_Getaways/",
   },
   {
     name: "U.S. State Capitals Quiz GUI",
-    description: "Launch a quiz on a Python tkinter window that tests you on the capitals of the 50 states.",
+    description:
+      "Launch a quiz on a Python tkinter window that tests you on the capitals of the 50 states.",
     tags: ["Python", "tkinter", "Selenium"],
-    githubLink: "https://github.com/RudraPatel2003/US_State_Capitals_Quiz_GUI"
-  }
+    githubLink: "https://github.com/RudraPatel2003/US_State_Capitals_Quiz_GUI",
+  },
 ];
