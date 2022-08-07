d02687760668886fe5e6ba25f56dce8823d9433d
Rudra Patel <patelrudra2003@gmail.com>
2022-08-07 10:48:00 -0500

    Adds LinkedIn link

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export const contactInformation = {
   email: "patelrudra2003@gmail.com",
   github: "https://github.com/RudraPatel2003",
   instagram: "https://www.instagram.com/pateltherudra/",
-  linkedIn: null
-}
+  linkedIn: "https://www.linkedin.com/in/rudra-j-patel/",
+};
 
 export const projects = [
   {
